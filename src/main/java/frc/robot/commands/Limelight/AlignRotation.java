94910a51066765564d3e501e01e07bd74c65dce7
tlsd-robotics <114959603+tlsd-robotics@users.noreply.github.com>
2023-02-18 14:07:08 -0800

    Implemented AlignRotation

--- src/main/java/frc/robot/commands/Limelight/AlignRotation.java
+++ src/main/java/frc/robot/commands/Limelight/AlignRotation.java
@@ -4,22 +4,42 @@
 
 package frc.robot.commands.Limelight;
 
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Limelight;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class AlignRotation extends CommandBase {
+
+  PIDController rotation = new PIDController(0.05, 0, 0);
+  DrivetrainSubsystem drivetrain;
+  Limelight limelight;
+  DoubleSupplier x;
+  DoubleSupplier y;
+
   /** Creates a new ZeroRotation. */
-  public AlignRotation(DrivetrainSubsystem drivetrain) {
+  public AlignRotation(DrivetrainSubsystem drivetrain, Limelight limelight, DoubleSupplier x, DoubleSupplier y) {
+    this.drivetrain = drivetrain;
+    this.limelight = limelight;
+    this.x = x;
+    this.y = y;
     addRequirements(drivetrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    rotation.setTolerance(0.1);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    drivetrain.drive(new ChassisSpeeds(x.getAsDouble(), y.getAsDouble(), rotation.calculate(-1 * limelight.getHorizontalError(), 0)));
+  }
 
   // Called once the command ends or is interrupted.
   @Override
