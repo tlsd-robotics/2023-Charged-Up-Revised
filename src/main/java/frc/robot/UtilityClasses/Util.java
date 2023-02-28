0e27c3ab241c3d2ceb17fc8e9d3fe27db98e0eb2
tlsd-robotics <114959603+tlsd-robotics@users.noreply.github.com>
2023-02-27 20:16:59 -0800

    Added inRange() method

--- src/main/java/frc/robot/UtilityClasses/Util.java
+++ src/main/java/frc/robot/UtilityClasses/Util.java
@@ -5,4 +5,9 @@ public class Util {
     public static double map(double Value, double FromMin, double FromMax, double ToMin, double ToMax) {
         return (((Value - FromMin) / (FromMax - FromMin)) * (ToMax - ToMin)) + ToMin;
     }
+
+    //Returns true if value is between or equal to min and max
+    public static boolean inRange(double value, double min, double max) {
+        return (value <= max) && (value >= min);
+    }
 }
